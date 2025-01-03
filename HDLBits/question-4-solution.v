54cdc6d488724220ea44c861799a28fc00d2c6b0
JoelleDAmico <70987991+JoelleDAmico@users.noreply.github.com>
2025-01-03 18:27:21 -0500

    Four Wires Solution

---
+++ HDLBits/question-4-solution.v
@@ -0,0 +1,11 @@
+// Four Wires - Question 4 - HDLBits
+// Solution by: Joelle D'Amico
+
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    assign w = a;
+    assign x = b; 
+    assign y = b;
+    assign z = c;
+endmodule
